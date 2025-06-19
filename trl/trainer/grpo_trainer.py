f07085fec590cbb375466b2f181ffdc09acbe41f
Mert Toslali <toslali@ibm.com>
2025-06-19 14:30:17 -0400

    Fix train dataset for multi proc

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -399,6 +399,8 @@ class GRPOTrainer(Trainer):
         self.REUSE_BUFFER = deque() # FIFO for confirmed promising indices
         self.PROMISING_BUFFER = {} # Currently being explored for a second round, includes idx = [23,23] for example
         self.AVAILABLE_INDICES = set(range(len(train_dataset))) # all remaining datapoint indices to explore
+        print(f"[Rank {self.accelerator.process_index}] available indices {self.AVAILABLE_INDICES}")
+        print(0/0)
         self.ENABLE_EXPLORATION = args.smart_sampling
         self.EXPLORATION_BUDGET = args.num_generations # for now, num generations exploration because of vllm optimization (unique prompt)- todo however many copies you want 
         self.EXPLORATION_THRESHOLD = args.num_generations # observations before discarding
